186467385b98d1f0153af8eeef42c83c6d070577
EUNJI LEE <eunji@EUNJIui-MacBookAir.local>
2022-10-05 22:09:40 +0900

    feat: 일반 함수 사용 방법 정리

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-// let
-// const와 동일하나 값의 재할당이 가능하다.
+// 함수란?
+// 특정 기능을 모아둔 집합체
+// 또는 특정 계산된 값을 재사용하기 위해
 
-// let는 중복 선언이 안된다.
-let number = 10;
-let number = 10; // SyntaxError: Identifier 'number' has already been declared
-
-// let는 값 재할당이 가능하다.
-let age = 10;
-age = 20;
+// 함수 선언 방법
+// 매개변수(parameter)는 함수 호출시 값을 받아오는 변수명이라고 생각하면된다.
+function 함수명(매개변수) {
+    //... 함수 실행시 작동할 코드 내용
+  }
+  
+  // 함수에 대한 예제
+  // function greeting(username) {
+  //   console.log(`${username}님 안녕하세요.`);
+  // }
+  
+  const app = document.getElementById("app");
+  
+  function greeting(username) {
+    console.log(`${username}님 안녕하세요.`);
+    // 함수내의 값이 어딘가에 할당이 되려면 return 적어줘야 한다.
+    return `${username}님 안녕하세요.`;
+  }
+  
+  app.innerText = greeting("이은지");
+  
+  function sum(a, b) {
+    // 주의 사항으로 return 아래 줄에는 코드를 적지 않는다.
+    return a + b;
+  }
+  
+  const result = sum(10, 10);
+  console.log(result);

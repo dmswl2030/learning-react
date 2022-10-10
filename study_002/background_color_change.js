fd7a458cf1839119ae58e282bcc5b4af528e8c99
EUNJI LEE <eunji@EUNJIui-MacBookAir.local>
2022-10-10 22:42:08 +0900

    221010 feat: 스터디 한 내용 주석달고 정리

--- study_002/background_color_change.js
+++ study_002/background_color_change.js
@@ -1,14 +1,19 @@
-// 버튼을 누른다.
+//5가지의 랜덤한 배경이
+//버튼을 누를때마다 바뀐다
+
+// 1. 버튼을 누를 btn 변수 생성 
 const app = document.getElementById("app");
 const btn = document.querySelector(".btn");
 
-// 배경색은 5가지가있다.
+// 2. 배경색을 담을 배열 bgColor를 생성
 const bgColor = ["tomato", "hotpink", "red", "blue", "pupple"];
 
-// 버튼을 클릭하면 배경색 5가지중 한가지 색으로 변경된다.
+// 3. 버튼을 클릭하면 배경색 5가지중 한가지 색으로 변경된다.
 btn.addEventListener("click", () => {
-    //floor : 올림
-    const random = Math.floor(Math.random()*5);
-    app.style.backgroundColor = bgColor[random];
-});
+  
+  // 3-1. 5가지를 랜덤하게 계산할 변수 random 생성 (floor: 올림)
+  const random = Math.floor(Math.random()*5);
 
+  // 3-2. bgColor의 배열에 random변수를 담아 backgroundColor의 스타일을 바꿈
+  app.style.backgroundColor = bgColor[random];
+});
--- study_002/todo_list.js
+++ study_002/todo_list.js
@@ -1,25 +1,48 @@
+// input창에 텍스트 입력 후 등록 버튼을 누르면
+// 하단 목록에 입력한 값이 마지막에 노출
+
+// 1. 필요한 변수 생성하기
 const ul = document.querySelector("ul");
 const form = document.querySelector("form");
 const btn = document.querySelector("button");
 const input = document.querySelector("input");
 
+// 2. 목록을 담은 배열 todos 변수 생성
 let todos = ["밥먹기", "숙제하기", "공부하기"];
 
+// 3. 목록을 나타내는 printing 함수 생성
 function printing() {
+
+  // 3-1. todos를 반복하는 
   todos.map((todo) => {
+
+    // 3-2. html에 li 태그 생성 
     const li = document.createElement("li");
+
+    // 3-3. ul 태그의 자식요소로 li생성
     ul.appendChild(li);
+
+    // 3-4. li 태그에 todo 출력
     li.innerText = todo;
   });
 }
 
-// 버튼을 눌렀을 때 text 가져오기
+// 4. 버튼을 눌렀을 때 text 가져오기
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
+  // 4-1. todos배열에 input의 텍스트 값을 추가해줌
   todos = [...todos, input.value];
+
+  // 4-2. ul의 텍스트를 초기화
   ul.innerHTML = null;
+
+  // 4-3. printing 함수 호출
   printing();
+
+  // 4-4. input 입력값 초기화
   input.value = "";
 });
 
+// 5. printing 함수 호출
 printing();
